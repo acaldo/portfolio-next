6f60bad016d3d0d85cff114aef69976f4e20f1da
caldev <caldart.dev@gmail.com>
2024-01-17 17:11:28 -0300

    fixs

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,19 +47,17 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
+          Conectemos
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          Estoy actualmente buscando nuevas oportunidades, mi bandeja de entrada siempre está abierta. Ya sea que tengas una pregunta o solo quieras saludar, ¡haré todo lo posible por responderte!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/acaldo" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="https://www.linkedin.com/in/acaldart/.com" target="_blank">
+          <Link href="https://www.linkedin.com/in/acaldart/" target="_blank">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
@@ -76,7 +74,7 @@ const EmailSection = () => {
                 htmlFor="email"
                 className="text-white block mb-2 text-sm font-medium"
               >
-                Your email
+                Tu email
               </label>
               <input
                 name="email"
@@ -92,7 +90,7 @@ const EmailSection = () => {
                 htmlFor="subject"
                 className="text-white block text-sm mb-2 font-medium"
               >
-                Subject
+                Asunto
               </label>
               <input
                 name="subject"
@@ -100,7 +98,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="Asunto"
               />
             </div>
             <div className="mb-6">
@@ -108,20 +106,20 @@ const EmailSection = () => {
                 htmlFor="message"
                 className="text-white block text-sm mb-2 font-medium"
               >
-                Message
+                Mensaje
               </label>
               <textarea
                 name="message"
                 id="message"
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Let's talk about..."
+                placeholder="Hablemos..."
               />
             </div>
             <button
               type="submit"
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
-              Send Message
+              Enviar!
             </button>
           </form>
         )}
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,15 +7,15 @@ import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
-    title: "About",
+    title: "Sobre mi",
     path: "#about",
   },
   {
-    title: "Projects",
+    title: "Projectos",
     path: "#projects",
   },
   {
-    title: "Contact",
+    title: "Contacto",
     path: "#contact",
   },
 ];
